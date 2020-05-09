9ef0394c31a7368219a2f4ecf65062df3c42b18d
Simon-Neo <simonmatthew@naver.com>
2020-05-09 17:40:43 +0900

    Begin_Practice

---
+++ ModuelTest.py
@@ -0,0 +1,14 @@
+import os
+
+import termcolor
+import sys
+
+print("------------ os -------------")
+print(os.__file__)
+
+print("------------ termcolor -------------")
+print(termcolor.__file__)
+
+print("------------ Path -------------")
+
+print(sys.path)
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='practice_module',
+    version='1.0.0',
+    packages=['practice', 'practice.Test'],
+    url='www.github.com',
+    license='Free',
+    author='simon',
+    author_email='simonmatthew@naver.com',
+    description='Practice Practice'
+)
